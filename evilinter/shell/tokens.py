e9ff17f8d18b014d731d43f35d9c958d4340af2b
Damien Nadé <anvil.github@livna.org>
2020-03-15 14:46:36 +0100

    add singe quote related Token classes

--- evilinter/shell/tokens.py
+++ evilinter/shell/tokens.py
@@ -3,3 +3,15 @@ from ..tokens import Token
 
 class Comment(Token):
     pass
+
+
+class OpeningSingleQuote(Token):
+    pass
+
+
+class ClosingSingleQuote(Token):
+    pass
+
+
+class SingleQuotedString(Token):
+    pass
