c892457f245581a22a6287a538e645ae45f13170
Damien Nadé <anvil.github@livna.org>
2020-03-19 22:57:22 +0100

    implement ability to analyse some redirections, ! for command inversion, and glue altogether double quoting and [[

--- evilinter/shell/__init__.py
+++ evilinter/shell/__init__.py
@@ -1,15 +1,16 @@
 import re
 
-from ..lexer import Lexer
+from ..typing import TokenYielder
+from ._common import _CommonLexer
 from .tokens import *
+from .conditional import DoubleBracketConditionnalLexer
 
 
-class BashLexer(Lexer):
+class BashLexer(_CommonLexer):
 
-    __word_char = re.compile('[-a-zA-Z0-9+_]').match
-   
-    def __consume_word(self):
-        self.__consume(str.isalnum)
+    word_char = re.compile(r'[-a-zA-Z0-9_/]').fullmatch
+
+    variable_char = re.compile(r'[a-zA-Z0-9_]').fullmatch
 
     @staticmethod
     def __not_single_quote(char: str):
@@ -23,30 +24,77 @@ class BashLexer(Lexer):
         self.forward()
         yield ClosingSingleQuote
 
-    def double_quote(self):
-        pass
-
     def subshell(self):
         pass
 
     def expansion(self):
+        self.forward()
+        yield from self.consume_select(self.variable_char, Parameter)
+
+    def bracket(self):
+        if self.compare_word_ahead("[", "["):
+            return DoubleBracketConditionnalLexer(self.buffer)
+
+    def curly_bracket(self):
         pass
 
     def comment(self):
-        yield from self.consume_rest_of_line(Comment)
+        return self.consume_rest_of_line(Comment)
 
-    def __iter__(self):
-        last_token_position = None
+    def word(self) -> TokenYielder:
+        return self.consume_select(self.word_char, Token)
+
+    def redirection(self) -> TokenYielder:
+        if self.current == ">":
+            self.forward()
+            if self.current == "&":
+                self.forward()
+                yield OutputRedirectionToFD
+                yield from self.consume_select(str.isdigit, FileDescriptor)
+            elif self.current == ">":
+                raise NotImplementedError()
+            elif self.current == ">":
+                yield OutputRedirectionToFile
+                yield from self.consume_separator(WhiteSpace)
+                yield from self.word()
+        else:
+            raise NotImplementedError()
+
+    def bang(self) -> TokenYielder:
+        self.forward()
+        if self.current in self.SPACE_SEPARATOR:
+            yield CommandStatusInvertion
+            return
+
+    KEYWORDS = {"if", "for", "while", "until", "function", "case",
+                "declare", "typeset", "export", "wait", "set", "unset"}
+
+    def __iter__(self) -> TokenYielder:
         consumers = {"#": self.comment,
                      "'": self.single_quote,
                      '"': self.double_quote,
                      "(": self.subshell,
-                     "$": self.expansion}
+                     "$": self.expansion,
+                     "{": self.curly_bracket,
+                     "[": self.bracket,
+                     "\n": self.consume_eol,
+                     ">": self.redirection,
+                     "<": self.redirection,
+                     "!": self.bang}
+
+        last_token_position = None
         while last_token_position != self.buffer.position:
-            last_token_position = self.buffer.position
+            last_token_position = self.buffer.position.copy()
             try:
                 consumer = consumers[self.current]
             except KeyError:
-                pass
+                yield from self.consume_separator(WhiteSpace)
+                if self.word_char(self.current):
+                    yield from self.word()
             else:
-                yield from consumer()
+                try:
+                    yield from consumer()
+                except TypeError:
+                    pass
+        print("Incomplete parsing:")
+        print(self.buffer)
